f9ec549f1fd016783271481a46ea1aa6d79c359a
devnova777 <devnova0821@gmail.com>
2025-02-14 11:45:28 +0100

    redeploy

--- server/utils/APIError.js
+++
@@ -1,8 +0,0 @@
-export class APIError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
